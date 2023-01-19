280e2f0f8292342fabb560007bf2450a6aea3e28
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-01-19 15:10:08 +0100

    get number of repos by owner - initial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_MY_TOKEN,
+});
 
 function App() {
-  return <div className="App">react app</div>;
+  const [owner, setOwner] = useState("octokit");
+  const [repos, setRepos] = useState([]);
+
+  const getAllRepos = (owner) => {
+    octokit
+      .request(`GET /orgs/${owner}/repos`, {})
+      .then((result) => {
+        setRepos(result.data);
+      })
+      .catch((error) => console.log(error.status));
+  };
+
+  useEffect(() => {
+    getAllRepos(owner);
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>All {owner} repos: </h3>
+      <p>number of repos: {repos.length}</p>
+    </div>
+  );
 }
 
 export default App;
