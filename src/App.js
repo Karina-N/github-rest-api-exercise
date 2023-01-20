d0d2a3ebb457f16038a9b0ad968f32cf1375fae9
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-01-20 23:53:12 +0100

    get orgs count on github

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ const octokit = new Octokit({
 });
 
 function App() {
-  const [organization, setOrganization] = useState("octokit");
+  const [organization, setOrganization] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   const [repos, setRepos] = useState([]);
   const [biggestRepo, setBiggestRepo] = useState("");
+  const [organizationsCount, setOrganizationsCount] = useState(0);
 
   const getAllRepos = () => {
     octokit
@@ -25,20 +27,47 @@ function App() {
   };
 
   const getBiggestRepo = (repoList) => {
-    console.log(repoList);
     let biggestRepoSize = Math.max(...repoList.map((r) => r.size));
     let biggestRepository = repoList.find((r) => r.size === biggestRepoSize);
     setBiggestRepo(biggestRepository);
   };
 
+  const getNumberOfOrganizations = () => {
+    console.log("i'm in");
+    octokit
+      .request("GET /search/users?q=type%3Aorg", {})
+      .then((result) => {
+        console.log(result.data.total_count);
+        setOrganizationsCount(result.data.total_count);
+      })
+      .catch((error) => console.log(error.status));
+  };
+
   useEffect(() => {
     getAllRepos();
-  }, []);
+    getNumberOfOrganizations();
+  }, [organization]);
 
-  console.log(biggestRepo);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    setOrganization(e.target[0].value);
+  };
 
   return (
     <div className="App">
+      <p>Currently there are {organizationsCount} organizations on Github</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type organization"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+
       <h3>All {organization} repos: </h3>
 
       {repos.length > 0 && (
